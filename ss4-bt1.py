5069fea9e56153eebd47b00aca0642dcd996b642
Nhĩ  Đan <huynhhonhidan@gmail.com>
2026-05-27 16:03:02 +0700

    bt1

---
+++ ss4-bt1.py
@@ -0,0 +1,11 @@
+cost = int(input("Nhập tổng tiền hóa đơn ban đầu: "))
+
+if cost >= 500000:
+    money_desc = cost * 0.1
+
+int(money_desc)
+total_cost = cost - money_desc
+
+print("--- Hóa đơn thanh toán Rikkei Store ---")
+print(f"Số tiền được giảm giá: {money_desc} VND")
+print(f"Tổng tiền khách phải trả: {total_cost} VND")
