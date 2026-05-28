8c6fb08be0908217923256231ca34ff03748e5d1
poldayy <huynhhonhidan@gmail.com>
2026-05-28 12:24:20 +0700

    Update ss4-bt1.py

--- ss4-bt1.py
+++ ss4-bt1.py
@@ -2,7 +2,8 @@ cost = int(input("Nhập tổng tiền hóa đơn ban đầu: "))
 
 if cost >= 500000:
     money_desc = cost * 0.1
-
+else:
+    money_desc = 0
 int(money_desc)
 total_cost = cost - money_desc
 
